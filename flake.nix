58cc81301b088476b18a9d65b5fe1f4b27699597
Gab Fontes <hi@m7.rs>
2026-06-01 17:10:30 -0300

    feat: package with nix (#1)

---
+++ flake.nix
@@ -0,0 +1,20 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-26.05";
+    systems.url = "github:nix-systems/default";
+  };
+
+  outputs = {nixpkgs, systems, ...}: let
+    forEachSystem = nixpkgs.lib.genAttrs (import systems);
+  in {
+    packages = forEachSystem (system: rec {
+      openhfta = nixpkgs.legacyPackages.${system}.callPackage ./default.nix {};
+      default = openhfta;
+
+      difftests = openhfta.override { withDiffTests = true; };
+    });
+    overlays.default = final: _prev: {
+      openhfta = final.callPackage ./default.nix {};
+    };
+  };
+}
